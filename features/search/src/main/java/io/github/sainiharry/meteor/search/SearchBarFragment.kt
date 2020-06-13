50df7e0d01c20af6148170525194947c5576d1d1
Harsimran Saini <harry.singh@mailbox.org>
2020-06-13 11:30:51 -0400

    Add search bar fragment

---
+++ features/search/src/main/java/io/github/sainiharry/meteor/search/SearchBarFragment.kt
@@ -0,0 +1,7 @@
+package io.github.sainiharry.meteor.search
+
+import io.github.sainiharry.meteor.commonfeature.BaseFragment
+
+class SearchBarFragment : BaseFragment() {
+
+}
---
+++ features/search/src/main/java/io/github/sainiharry/meteor/search/SearchViewModel.kt
@@ -0,0 +1,8 @@
+package io.github.sainiharry.meteor.search
+
+import io.github.sainiharry.meteor.commonfeature.BaseViewModel
+
+class SearchViewModel : BaseViewModel() {
+
+
+}
