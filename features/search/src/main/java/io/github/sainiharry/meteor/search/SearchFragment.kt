0637a86f369ab05846d3eb9bd292438f95efd78b
Harsimran Saini <harry.singh@mailbox.org>
2020-06-13 12:01:44 -0400

    Add SearchFragment

---
+++ features/search/src/main/java/io/github/sainiharry/meteor/search/SearchFragment.kt
@@ -0,0 +1,34 @@
+package io.github.sainiharry.meteor.search
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import io.github.sainiharry.meteor.search.databinding.FragmentSearchBinding
+
+class SearchFragment : Fragment() {
+
+    private val model by activityViewModels<SearchViewModel>()
+
+    private lateinit var binding: FragmentSearchBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentSearchBinding.inflate(inflater, container, false)
+        binding.model = model
+        binding.lifecycleOwner = viewLifecycleOwner
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        
+        binding.searchInputLayout.setEndIconOnClickListener {
+            model.handleSearchDone()
+        }
+    }
+}
--- features/search/src/main/java/io/github/sainiharry/meteor/search/SearchViewModel.kt
+++ features/search/src/main/java/io/github/sainiharry/meteor/search/SearchViewModel.kt
@@ -1,8 +1,19 @@
 package io.github.sainiharry.meteor.search
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import io.github.sainiharry.meteor.commonfeature.BaseViewModel
 
 class SearchViewModel : BaseViewModel() {
 
+    val searchTextInput = MutableLiveData<String>()
 
+    private val _searchText = MutableLiveData<String>()
+
+    val searchText: LiveData<String>
+        get() = _searchText
+
+    fun handleSearchDone() {
+        _searchText.value = searchTextInput.value
+    }
 }
