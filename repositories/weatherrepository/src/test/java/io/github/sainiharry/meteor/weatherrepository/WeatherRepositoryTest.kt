ba2744fb9afbaaf4a553d5d1b131f6572c943333
Harsimran Saini <harry.singh@mailbox.org>
2020-06-14 12:49:26 -0400

    Add test for weather repository

---
+++ repositories/weatherrepository/src/test/java/io/github/sainiharry/meteor/weatherrepository/WeatherRepositoryTest.kt
@@ -0,0 +1,100 @@
+package io.github.sainiharry.meteor.weatherrepository
+
+import io.github.sainiharry.meteor.common.Weather
+import io.github.sainiharry.meteor.weatherrepository.database.CurrentWeatherModel
+import io.github.sainiharry.meteor.weatherrepository.database.WeatherDao
+import io.github.sainiharry.meteor.weatherrepository.database.WeatherDatabase
+import io.github.sainiharry.meteor.weatherrepository.network.*
+import io.reactivex.Single
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.Mockito.*
+import org.mockito.junit.MockitoJUnitRunner
+
+@RunWith(MockitoJUnitRunner::class)
+class WeatherRepositoryTest {
+
+    @Mock
+    private lateinit var weatherDatabase: WeatherDatabase
+
+    @Mock
+    private lateinit var openWeatherService: OpenWeatherService
+
+    @Mock
+    private lateinit var weatherDao: WeatherDao
+
+    private lateinit var weatherRepository: WeatherRepository
+
+    val cityName = "London"
+
+    @Before
+    fun setup() {
+        `when`(weatherDatabase.weatherDao()).thenReturn(weatherDao)
+        weatherRepository = WeatherRepositoryImpl(openWeatherService, weatherDatabase)
+    }
+
+    @Test
+    fun testFetchCurrentWeatherForCity() {
+        val mockWeatherResponse = mockWeatherResponse()
+        val weather = mockWeatherResponse.flatten()
+        val currentWeatherModel = CurrentWeatherModel(weather)
+        `when`(openWeatherService.getCurrentWeather(cityName)).thenReturn(
+            Single.just(
+                mockWeatherResponse
+            )
+        )
+
+        val subscription = weatherRepository.fetchCurrentWeather(cityName).test()
+        subscription.assertNoErrors()
+        subscription.assertValue(weather)
+
+        verify(openWeatherService).getCurrentWeather(cityName)
+        verify(weatherDao).insertCurrentWeather(currentWeatherModel)
+        verifyNoMoreInteractions(openWeatherService)
+        verifyNoMoreInteractions(weatherDao)
+    }
+
+    @Test
+    fun testFetchWeatherForLocation() {
+        val location = 23.3 to 48.3
+        val mockWeatherResponse = mockWeatherResponse()
+        val weather = mockWeatherResponse.flatten()
+        val currentWeatherModel = CurrentWeatherModel(weather)
+        `when`(openWeatherService.getCurrentWeather(location.first, location.second)).thenReturn(
+            Single.just(
+                mockWeatherResponse
+            )
+        )
+
+        val subscription = weatherRepository.fetchCurrentWeather(location.first, location.second).test()
+        subscription.assertNoErrors()
+        subscription.assertValue(weather)
+
+        verify(openWeatherService).getCurrentWeather(location.first, location.second)
+        verify(weatherDao).insertCurrentWeather(currentWeatherModel)
+        verifyNoMoreInteractions(openWeatherService)
+        verifyNoMoreInteractions(weatherDao)
+    }
+
+    private fun mockWeatherResponse() = CurrentWeatherResponse(
+        cityName.hashCode().toLong(),
+        cityName,
+        listOf(WeatherResponse(cityName.hashCode().toLong(), "Clear", "04d")),
+        WeatherInfoResponse(23.2f, 43.2f, 85.3f),
+        WeatherSysResponse("US")
+    )
+
+    private fun mockWeather(
+        cityName: String = "Montreal",
+        id: Long = cityName.hashCode().toLong(),
+        main: String = "Clear",
+        icon: String = "0445",
+        cityId: Long = cityName.hashCode().toLong(),
+        temp: Float = 38.3f,
+        maxTemp: Float = 30.3f,
+        minTemp: Float = 43.1f,
+        country: String = "US"
+    ) = Weather(id, main, icon, cityId, cityName, temp, maxTemp, minTemp, country)
+}
