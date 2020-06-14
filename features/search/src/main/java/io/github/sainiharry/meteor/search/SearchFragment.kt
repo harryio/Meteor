fd28ad8fc2ae1e60aae2076f737755cb5ecba1e7
Harsimran Saini <harry.singh@mailbox.org>
2020-06-14 10:30:45 -0400

    Handle search text input ime action

--- features/search/src/main/java/io/github/sainiharry/meteor/search/SearchFragment.kt
+++ features/search/src/main/java/io/github/sainiharry/meteor/search/SearchFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.EditorInfo
 import androidx.fragment.app.activityViewModels
 import com.google.android.material.transition.MaterialFadeThrough
 import com.google.android.material.transition.MaterialSharedAxis
@@ -34,6 +35,14 @@ class SearchFragment : BaseFragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        binding.searchText.setOnEditorActionListener { _, actionId, _ ->
+            if (actionId == EditorInfo.IME_ACTION_SEARCH) {
+                model.handleSearchDone()
+                true
+            } else {
+                false
+            }
+        }
         binding.searchInputLayout.setEndIconOnClickListener {
             model.handleSearchDone()
         }
