548e34f236aa3b2740997aec1fb186669d5d07d0
Harsimran Saini <harry.singh@mailbox.org>
2020-06-15 12:25:58 -0400

    Add documentation for network module

--- network/src/main/java/io/github/sainiharry/meteor/network/NetworkInteractor.kt
+++ network/src/main/java/io/github/sainiharry/meteor/network/NetworkInteractor.kt
@@ -8,7 +8,12 @@ import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.moshi.MoshiConverterFactory
 
-class NetworkInteractor(val scheduler: Scheduler) {
+/**
+ * Provides basic implementation of network stack. Provides a Retrofit instance with shared resources
+ * which can be used by clients to create network service classes. Uses Moshi as library for
+ * serialization/deserialization of network request and responses.
+ */
+class NetworkInteractor private constructor(private val scheduler: Scheduler) {
 
     private val retrofitMap = mutableMapOf<Api, Retrofit>()
 
@@ -16,6 +21,13 @@ class NetworkInteractor(val scheduler: Scheduler) {
 
         private var networkInteractor: NetworkInteractor? = null
 
+        /**
+         * Provides cached instance of retrofit per api
+         * @param api Implementation of [Api] interface
+         * @param scheduler Default [Scheduler] on which network calls will be performed on. The
+         * network calls will be scheduled on this scheduler
+         * @return Cached instance of [Retrofit] for this specified [Api]
+         */
         fun getRetrofit(api: Api, scheduler: Scheduler): Retrofit {
             if (networkInteractor == null) {
                 networkInteractor = NetworkInteractor(scheduler)
@@ -53,11 +65,32 @@ class NetworkInteractor(val scheduler: Scheduler) {
     }
 }
 
+/**
+ * Network stack for a network Api
+ */
 interface Api {
 
+    /**
+     * Provides basic implementation of [Retrofit.Builder]. This Builder is used for creating
+     * cached instances of [Retrofit] for this particular [Api]
+     * @return instance of [Retrofit.Builder]
+     */
     fun buildRetrofit(): Retrofit.Builder
 
+    /**
+     * Provides basic implementation of [OkHttpClient.Builder]. The returned builder is used for
+     * creating networking layer of cached instances of [Retrofit] for this particular [Api]
+     * @param okHttpBuilder a shared instance of [OkHttpClient.Builder] among different Api's
+     * @return instance of [OkHttpClient.Builder]
+     */
     fun buildOkHttpClient(okHttpBuilder: OkHttpClient.Builder): OkHttpClient.Builder = okHttpBuilder
 
+    /**
+     * Provides basic implementation of [Moshi.Builder]. The returned builder is used for adding
+     * serialization/deserialization capabilities to the cached instance of [Retrofit] for this
+     * particular [Api]
+     * @param moshiBuilder a shared instance of [Moshi.Builder] among different Api's
+     * @return instance of [Moshi.Builder]
+     */
     fun buildMoshi(moshiBuilder: Moshi.Builder): Moshi.Builder = moshiBuilder
 }
