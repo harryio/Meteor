005b20d8add22c839045a54e8ddcb2c23f11f97b
Harsimran Saini <harry.singh@mailbox.org>
2020-07-01 14:55:52 -0400

    Add tests for search repository

---
+++ repositories/searchrepository/src/test/java/io/github/sainiharry/searchrepository/SearchRepositoryTest.kt
@@ -0,0 +1,40 @@
+package io.github.sainiharry.searchrepository
+
+import com.squareup.sqldelight.sqlite.driver.JdbcSqliteDriver
+import kotlinx.coroutines.test.TestCoroutineDispatcher
+import kotlinx.coroutines.test.runBlockingTest
+import org.junit.Assert.assertEquals
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.junit.MockitoJUnitRunner
+
+@RunWith(MockitoJUnitRunner::class)
+class SearchRepositoryTest {
+
+    private lateinit var searchRepository: SearchRepository
+
+    @Before
+    fun setup() {
+        val driver = JdbcSqliteDriver(JdbcSqliteDriver.IN_MEMORY)
+        Database.Schema.create(driver)
+        val database = Database(driver)
+
+        searchRepository = SearchRepositoryImpl(database, TestCoroutineDispatcher())
+    }
+
+
+    @Test
+    fun testGetSearchQueries() = runBlockingTest {
+        val cities = listOf("Montreal", "New York", "London")
+        cities.forEach {
+            searchRepository.handleSearchQuery(it)
+        }
+
+        val databaseCities = searchRepository.getSearchQueries().map {
+            it.searchQuery
+        }.reversed()
+
+        assertEquals(cities, databaseCities)
+    }
+}
